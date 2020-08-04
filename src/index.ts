b1b2a203ddbf6c22816e2b5dba6a1a1bd2f691d5
wangmeng27 <wangmeng27@58.com>
2020-08-04 15:53:08 +0800

    docs(docs): update gh-pages

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: wm
+ * @Date: 2020-08-04 15:51:01
+ * @LastEditTime: 2020-08-04 15:52:46
+ * @LastEditors: wm
+ * @Description: In User Settings Edit
+ * @FilePath: /Xhooks/src/index.ts
+ */
+export  {default as useToggle} from './useToggle'
