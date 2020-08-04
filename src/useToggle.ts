ad2c6603e7d2b691af5f155b6a8d33392c968dc1
wangmeng27 <wangmeng27@58.com>
2020-08-04 15:35:37 +0800

    docs: init
    init the project

--- src/useToggle.ts
+++ src/useToggle.ts
@@ -1,4 +1,12 @@
-import {useState, useCallback} from 'react'
+/*
+ * @Author: your name
+ * @Date: 2020-07-28 16:46:09
+ * @LastEditTime: 2020-08-04 15:34:03
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /Xhooks/src/useToggle.ts
+ */
+import { useState, useCallback } from 'react'
 
 type SetTrue = () => void
 type SetFalse = () => void
@@ -9,11 +17,8 @@ export type useToggle = [boolean, SetTrue, SetFalse, Toggle, SetValue]
 
 export default function useToggle(initialValue: boolean) {
   const [value, setValue] = useState(initialValue)
-
   const setTrue = useCallback(() => setValue(true), [])
   const setFalse = useCallback(() => setValue(false), [])
   const toggle = useCallback(() => setValue(value => !value), [])
-
   return [value, setTrue, setFalse, toggle, setValue]
 }
-
